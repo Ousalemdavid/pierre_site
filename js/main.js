9f75df54c04b7501dac7b9ccb0b9db075b906aaa
Ousalem.david@netc.fr <you@example.com>
2017-07-04 10:48:21 +0200

    voila mon pierre feuille ciseaux de la semaine

---
+++ js/main.js
@@ -0,0 +1,183 @@
+//
+// var joueur = prompt('Choissisez vous Pierre , Feuille ou Ciseaux ?')
+// var ordi = Math.random();
+// var resultjoueur = ('Votre choix : ');
+// var resultordi = ('choix de l\'ordinateur : ');
+//
+// var score
+// if (ordi < 0.34) {
+//   ordi = "pierre";
+// }
+//
+//
+//
+// else if (ordi <= 0.67) {
+//   ordi = "feuille";
+// }
+//
+//
+// else {
+//   ordi = "ciseaux";
+// }
+//
+// console.log(resultjoueur + joueur);
+// console.log(resultordi + ordi);
+//
+//  var compare = function (joueur, ordi)
+//  {
+//
+//    if  (joueur === ordi)
+//   {
+//     return "égalité !";
+//   }
+//
+//
+//
+//
+//
+//
+//    else if (joueur === "pierre")
+//      {
+//        if (ordi === "ciseaux")
+//          {
+//            return "pierre gagne"
+//          }
+//        else {
+//          return  " feuille gagne "
+//        }
+//
+//
+//
+//      }
+//     else if  (joueur ==="feuille")
+//     {
+//       if  (ordi ==="pierre")
+//         {
+//           return " feuille gagne"
+//         }
+//       else
+//         {
+//           return  " ciseaux gagne"
+//         }
+//     }
+//
+//
+//
+//
+//    else if (joueur ==="ciseaux")
+// {
+//   if (ordi ==="pierre")
+//     {
+//       return "pierre gagne"
+//     }
+//
+// else
+//   {
+//     return  "ciseaux gagne "
+//   }
+// }
+//  }
+//
+// console.log(compare (joueur , ordi));
+
+
+
+
+var compteur1 = 0;
+var compteur2 = 0;
+
+
+
+  while (compteur1 <3 && compteur2 <3) {
+var joueur = prompt('Choissisez vous Pierre , Feuille ou Ciseaux ?')
+var ordi = Math.random();
+var resultjoueur = ('Votre choix : ');
+var resultordi = ('choix de l\'ordinateur : ');
+
+
+if (ordi < 0.34) {
+  ordi = "pierre";
+}
+
+
+
+else if (ordi <= 0.67) {
+  ordi = "feuille";
+}
+
+
+else {
+  ordi = "ciseaux";
+}
+
+// console.log(resultjoueur + joueur);
+//console.log(resultordi + ordi);
+
+ var compare = function (joueur, ordi)
+ {
+
+   if  (joueur === ordi)
+  {
+
+    return "égalité !";
+  }
+
+
+
+
+
+
+   else if (joueur === "pierre")
+     {
+       if (ordi === "ciseaux")
+         {
+           compteur1++;
+           return "pierre gagne"
+         }
+       else {
+         compteur2++;
+         return  " feuille gagne "
+       }
+
+
+
+     }
+    else if  (joueur ==="feuille")
+    {
+      if  (ordi ==="pierre")
+        {
+          compteur1++;
+          return " feuille gagne"
+        }
+      else
+        {
+          compteur2++;
+          return  " ciseaux gagne"
+        }
+    }
+
+
+
+
+   else if (joueur ==="ciseaux")
+{
+  if (ordi ==="pierre")
+    {
+      compteur2++;
+      return "pierre gagne"
+    }
+
+else
+  {
+    compteur1++;
+    return  "ciseaux gagne "
+  }
+}
+ }
+
+alert(resultjoueur + joueur);
+alert(resultordi + ordi);
+alert(compare (joueur , ordi));
+console.log(compteur1 , compteur2);
+
+}
